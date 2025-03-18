f3617be8e3d4e724d97ff325819ed1f8eb567efe
harshal0804 <gadre.sharshal@gmail.com>
2025-03-18 22:08:33 +0530

    Update server.js
    ig its working

--- server.js
+++ server.js
@@ -13,13 +13,6 @@ const io = new Server(server, {
         methods: ["GET", "POST"]
     }
 });
-io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-    });
-});
 
 let activeSessions = {};
 
